ed22d26a6f107e1f2b16994af86f8b7915ea89eb
Matthew Tamayo <matthew@kryptnostic.com>
2014-12-12 02:32:29 -0800

    Initial Commit

---
+++ MultivariatePolynomialFunction.cpp
@@ -0,0 +1,9 @@
+//
+//  MultivariatePolynomialFunction.cpp
+//  krypto
+//
+//  Created by Matthew Tamayo on 12/12/14.
+//  Copyright (c) 2014 Kryptnostic. All rights reserved.
+//
+
+#include "MultivariatePolynomialFunction.h"
