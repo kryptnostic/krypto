9665cec5ccc06efc981c9afdebefe66cff31f344
Matthew Tamayo <matthew@kryptnostic.com>
2015-03-16 00:41:01 -0700

    Xcode and gradle working

--- src/mainTest/cpp/test_main.cpp
+++
@@ -1,8 +0,0 @@
-#include "../../../contrib/gtest/gtest.h"
-
-using namespace testing;
-
-int main(int argc, char **argv) {
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-}
--- src/mainTest/cpp/test_trivial.cpp
+++
@@ -1,9 +0,0 @@
-#include "../../../contrib/gtest/gtest.h"
-
-using namespace testing;
-
-TEST(OperatorTests, test_minus) {
-	ASSERT_TRUE(2 + 0 == 2);
-	ASSERT_TRUE(0 - -2 == 2);
-	ASSERT_TRUE(2 - 2 == 0);
-}
