4704f21096623cb83f8cef444085a7c5d6c7e8aa
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2015-06-26 10:27:10 -0700

    Put libraries in the right location.

--- buildGoogleTest.sh
+++ buildGoogleTest.sh
@@ -1,1 +1,1 @@
-cd gtest-1.7.0 && cmake . && make && mv libgtest.a ../krypto-lib/libs/googleTest/1.7.0/lib/linux/ 
+cd gtest-1.7.0 && cmake . && make && mv libgtest.a krypto-lib/libs/googleTest/1.7.0/lib/linux/ 
