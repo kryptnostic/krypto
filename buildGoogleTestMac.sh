53d3ddb838be8d7be2b2db2bacd4aea08dab65a6
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2015-06-26 18:01:26 -0700

    Removed google test and other stuff from root.

--- buildGoogleTestMac.sh
+++ buildGoogleTestMac.sh
@@ -1,1 +1,1 @@
-cd gtest-1.7.0 && cmake . && make && mv libgtest.a ../krypto-lib/libs/googleTest/1.7.0/lib/osx/
+cd gtest-1.7.0 && cmake . && make && cp libgtest.a ../krypto-lib/libs/googleTest/1.7.0/lib/osx/
