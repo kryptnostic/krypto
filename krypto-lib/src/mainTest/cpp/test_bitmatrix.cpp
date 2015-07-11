8d88884769a134d63154be581908a3f79a9a4704
howpenghui <phow1@stanford.edu>
2015-07-10 18:03:20 -0700

    Test in progress

--- krypto-lib/src/mainTest/cpp/test_bitmatrix.cpp
+++ krypto-lib/src/mainTest/cpp/test_bitmatrix.cpp
@@ -11,6 +11,8 @@
 #include <string>
 using namespace testing;
 
+#define N 1
+
 TEST(BitMatrixTests, testRandomMatrix) {
 	BitMatrix<1> m = BitMatrix<1>::randomMatrix(64);
     BitVector<1> v = BitVector<1>::randomVector();
@@ -36,9 +38,14 @@ TEST(BitMatrixTests, testRandomMatrix) {
 	
 }
 
+TEST(BitMatrixTests, testAssignment){
+	BitMatrix<N> A = BitMatrix<N>::randomInvertibleMatrix(N<<6);
+	A.set(0,0), A.set(1,1), A.set((N<<6)-1,(N<<6)-1);
+	ASSERT_TRUE(A.get(0,0) && A.get(1,1) && A.get((N<<6)-1,(N<<6)-1));
+}
+
 TEST(BitMatrixTests, testSolve) {
  ASSERT_TRUE( 1+5 == 6 );
- 	const int N = 1;
 	BitVector<N> v = BitVector<N>::randomVector();
 	BitMatrix<N> M = BitMatrix<N>::randomInvertibleMatrix(N << 6);
 	BitVector<N> x = M.solve(v);
@@ -48,7 +55,6 @@ TEST(BitMatrixTests, testSolve) {
 
 TEST(BitMatrixTests, testInv) {
 	ASSERT_TRUE(1+5 == 6);
-	const int N = 1;
 	BitMatrix<N> M = BitMatrix<N>::randomInvertibleMatrix(N << 6);
 	BitMatrix<N> Mi = M.inv();
 	BitMatrix<N> Il = M*Mi;
@@ -58,4 +64,9 @@ TEST(BitMatrixTests, testInv) {
 	BitVector<N> x = BitVector<N>::randomVector();
 	BitVector<N> mix = Mi*x;
 	ASSERT_TRUE(mix.equals(M.solve(x)));
+}
+
+TEST(BitMatrixTests, testIdentity){
+	BitMatrix<N> I = BitMatrix<N>::squareIdentityMatrix();
+	ASSERT_TRUE(I.rref().isIdentity());
 }
