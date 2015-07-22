d0966497f39db038eeab3a046de0e9a277508683
howpenghui <phow1@stanford.edu>
2015-07-22 16:34:05 -0700

    added BridgeKey class, added TODO to privateKey

---
+++ krypto-lib/src/main/cpp/BridgeKey.h
@@ -0,0 +1,34 @@
+/*Accessed by client, used to generate components of the public keys*/
+#ifndef krypto_BridgeKey_h
+#define krypto_BridgeKey_h
+
+#include <assert.h>
+#include "PrivateKey.h"
+#include "BitMatrix.h"
+
+using namespace std;
+
+//TODO: make BridgeKey a friend class of PrivateKey
+
+template<unsigned int N, unsigned int L>
+class BridgeKey{
+public:
+	//BridgeKey(){}
+	BridgeKey(PrivateKey<N,L> pk, BitMatrix<N> K) : 
+	_R(BitMatrix<N>::randomInvertibleMatrix(N<<6)),
+	_BKBi(pk.getB() * K * pk.getB().inv()),
+	_BKBiAi(_BKBi * pk.getA().inv()),
+	_ARAi(pk.getA() * _R * pk.getA().inv())
+	{
+	}	
+
+private:
+	BitMatrix<N> _R; //TODO: delegate the random matrix generation task to some other class?
+	BitMatrix<N> _BKBi; 
+	BitMatrix<N> _BKBiAi;
+	BitMatrix<N> _ARAi;
+	PolynomialFunctionTupleChain<2*N,L> _g_u; //obsfucated chain for unary operations
+	PolynomialFunctionTupleChain<3*N,L> _g_b; //obsfucated chain for binary operations
+};
+
+#endif
--- krypto-lib/src/main/cpp/PrivateKey.h
+++ krypto-lib/src/main/cpp/PrivateKey.h
@@ -1,3 +1,4 @@
+/*Accessed by client*/
 #ifndef krypto_PrivateKey_h
 #define krypto_PrivateKey_h
 
@@ -11,6 +12,7 @@ using namespace std;
 /*length of plaintext = N*2^6; length of obfuscation chain = L*/
 template<unsigned int N, unsigned int L>
 class PrivateKey {
+//friend class BridgeKey;
 public:
 	PrivateKey():
 		_A(BitMatrix<N>::randomInvertibleMatrix(N<<6)),
@@ -36,11 +38,37 @@ public:
 		BitVector<N> fAix2 = _f(Aix2);
 		return _B.solve(x1 ^ (Aix2 ^ fAix2)); 
 	}
+
+/*protected:*/
+	/****************TODO: make the following protected functions only accessible to BridgeKey*************/
+	const BitMatrix<N> getA() const{
+		return _A;
+	}
+
+	const BitMatrix<N> getB() const{ 
+		return _B;
+	}
+
+	const BitMatrix<2*N> getM() const{
+		return _M;
+	}
+
+	const PolynomialFunctionTupleChain<N,L> getf() const{
+		return _f;
+	}
+
+	const vector<BitMatrix<2*N> > getUnaryObfChain() const{
+		return _C_u;
+	}
+	
+	const vector<BitMatrix<3*N> > getBinaryObfChain() const{
+		return _C_b;
+	}
+	/****************** end of TODO ******************************/
 private:
 	BitMatrix<N> _A, _B; //SL_n(F_2)
 	BitMatrix<2*N> _M; //SL_{2n}(F_2)
 	PolynomialFunctionTupleChain<N,L> _f; //{f_1,...,f_L} random quadratic function tuples
-	vector<MultivariatePolynomialFunction<N,N> > _v;
 	vector<BitMatrix<2*N> > _C_u; //chain of obfuscation matrix for unary operations
 	vector<BitMatrix<3*N> > _C_b; //chain of obfuscation matrix for binary operations
 	void generateObfuscationMatrixChains(){ //generates C_{u1},...,C_{uL} and C_{b1},...,C_{bL}
