6c0ea43c6c91b86a9ed91dca226bd8667b5e75b8
howpenghui <phow1@stanford.edu>
2015-07-24 16:02:20 -0700

    Added functionality of f(*); delegated padding to functional evaluation

--- krypto-lib/src/main/cpp/MultivariateQuadraticFunctionTuple.h
+++ krypto-lib/src/main/cpp/MultivariateQuadraticFunctionTuple.h
@@ -13,19 +13,22 @@ public:
 	_contributions(contributions){
 	}
 
+	//contributions A =  F_2{(n_i+1)C2 \times n_o}, so that f(x) = A^T\tilde{x}
 	const static MultivariateQuadraticFunctionTuple<NUM_INPUTS, NUM_OUTPUTS> randomMultivariateQuadraticFunctionTuple(){
-		BitMatrix<NUM_OUTPUTS> contributions = BitMatrix<NUM_OUTPUTS>::randomMatrix(monomialCount);
-		if(monomialCount == paddedMonomialCount) return contributions;
-		BitMatrix<NUM_OUTPUTS> zero = BitMatrix<NUM_OUTPUTS>::zeroMatrix(32);
-		return BitMatrix<NUM_OUTPUTS>::aug_v(contributions, zero);
+		MultivariateQuadraticFunctionTuple<NUM_INPUTS, NUM_OUTPUTS> f(BitMatrix<NUM_OUTPUTS>::randomMatrix(monomialCount));
+		return f;
 	}
-/*
-	//implementation in progress
+
+	const BitMatrix<NUM_OUTPUTS> getPaddedContribution() const{
+		if(!needPadding) return _contributions;
+		return BitMatrix<NUM_OUTPUTS>::aug_v(_contributions, BitMatrix<NUM_OUTPUTS>::zeroMatrix(32));
+	}
+
     const BitVector<NUM_OUTPUTS> operator()(const BitVector<NUM_INPUTS> & input) const {
-		int TRANSFORMED_INPUT = paddedMonomialCount/64;
+		const unsigned int TRANSFORMED_INPUT = paddedMonomialCount/64;
 		BitVector<TRANSFORMED_INPUT> t = BitVector<TRANSFORMED_INPUT>::zeroVector();
 		int count = 0;
-		for(int i = 0; i < numInputBits; ++i){
+		for(int i = 0; i < numInputBits; ++i){ //building (padded) \tilde{x}
 			t[count] = input[i];//x_i
 			++count;
 			for(int j = i+1; j < numInputBits; ++j){
@@ -33,16 +36,17 @@ public:
 				++count;
 			}
 		}
-		BitMatrix<
-		return _paddedContributions ** t; //todo: implement ** (M**x := M^T() * x)
+		BitMatrix<NUM_OUTPUTS> paddedContributions = getPaddedContribution();
+		return paddedContributions.tMult(t); 
 	}
-*/
+
 private:
 	BitMatrix<NUM_OUTPUTS> _contributions;
+	static const bool needPadding = (NUM_INPUTS % 2 == 0);
 	static const unsigned int numInputBits = (NUM_INPUTS << 6);
 	static const unsigned int numOutputBits = (NUM_OUTPUTS << 6);
 	static const unsigned int monomialCount = numInputBits + ((numInputBits * (numInputBits - 1)) >> 1);
-	static const unsigned int paddedMonomialCount = (monomialCount % 64 == 0) ? monomialCount : monomialCount + 32;
+	static const unsigned int paddedMonomialCount = needPadding ? monomialCount : monomialCount + 32;
 };
 
 #endif
