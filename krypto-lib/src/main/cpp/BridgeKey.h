2e6ca08409f0838c63e9860270fd54a9d8a10d69
Robin Zhang <robinz316@gmail.com>
2015-07-28 13:42:38 -0700

    BK: implement AND_z & AND_Sk, rename others to Pk, Qk

--- krypto-lib/src/main/cpp/BridgeKey.h
+++ krypto-lib/src/main/cpp/BridgeKey.h
@@ -99,9 +99,25 @@ public:
 
 /* AND */
 
-	const BitVector<2*N> get_AND_z() const{
-		//to be implemented
-		return BitVector<2*N>::randomVector();
+	const BitMatrix<N> get_AND_z() const{
+		//untested!
+		BitMatrix<2*N> X = get_AND_X();
+		BitMatrix<3*N> Y1 = get_AND_Y1();
+		BitMatrix<3*N> Y2 = get_AND_Y2();
+		BitMatrix<3*N> Y3 = get_AND_Y3();
+
+		BitMatrix<N> contrib;
+		for (int level = 0; level < 2*N; ++level) {
+			contrib = BitMatrix::aug_v(contrib, get_AND_Pk(level, X, Y2)); //add P_k's
+		}
+		for (int level = 0; level < 2*N; ++level) {
+			contrib = BitMatrix::aug_v(contrib, get_AND_Qk(level, X, Y1)); //add Q_k's
+		}
+		for (int level = 0; level < 3*N; ++level) {
+			contrib = BitMatrix::aug_v(contrib, get_AND_Sk(level, Y1, Y2)); //add S_k's
+		}
+
+		return contrib;
 	}
 
 	const BitMatrix<2*N> get_AND_Z1() const{
@@ -172,7 +188,7 @@ private:
 
 	//top chunk of contrib matrix for z
 	//level ranges from 0 to 64 * 2N - 1
-	const BitMatrix<N> get_AND_contrib_X_X_Y2(const int level, const BitMatrix<2*N> X, const BitMatrix<3*N> Y2) const{
+	const BitMatrix<N> get_AND_Pk(const int level, const BitMatrix<2*N> X, const BitMatrix<3*N> Y2) const{
 		//untested!
 		const int twoN = N << 7;
 		const int threeN = 3 * (N << 6);
@@ -200,11 +216,11 @@ private:
 
 	//middle chunk of contrib matrix for z
 	//level ranges from 0 to 64 * 2N - 1
-	const BitMatrix<N> get_AND_contrib_X_Y1(const int level, const BitMatrix<2*N> X, const BitMatrix<3*N> Y1) const{
+	const BitMatrix<N> get_AND_Qk(const int level, const BitMatrix<2*N> X, const BitMatrix<3*N> Y1) const{
 		//untested!
 		const int twoN = N << 7;
 		const int threeN = 3 * (N << 6);
-		BitMatrix<N> top = BitMatrix<N>::zeroMatrix(twoN - level); //TODO: check that ops with empty top matrix work
+		BitMatrix<N> top = BitMatrix<N>::zeroMatrix(twoN - level);
 
 		BitMatrix<N> bot = BitMatrix<N>::zeroMatrix(threeN);
 		for (int i = 0; i < threeN; i++) { //row within bottom block
@@ -219,11 +235,21 @@ private:
 
 	//bottom chunk of contrib matrix for z
 	//level ranges from 0 to 64 * 2N - 1
-	const BitMatrix<N> get_AND_contrib_Y1_Y2(const int level, const BitMatrix<3*N> Y1, const BitMatrix<3*N> Y2) const{
+	const BitMatrix<N> get_AND_Sk(const int level, const BitMatrix<3*N> Y1, const BitMatrix<3*N> Y2) const{
 		//to be implemented
 		const int threeN = 3 * (N << 6); //should be the number of coefficients (___ choose 2)
 
 		BitMatrix<N> contrib = BitMatrix<N>::zeroMatrix(threeN);
+		for (int j = 0; j < N; ++j) { //cols
+			bool prod = Y1.get(j, level) && Y2.get(j, level); //first row
+			contrib.set(0, j, prod);
+
+			for (int i = 1; i < threeN - level; ++i) { //rows
+				bool prod1 = Y1.get(j, level) && Y2.get(j, level + i);
+				bool prod2 = Y1.get(j, level + i) && Y2.get(j, level);
+				contrib.set(i, j, prod1 ^ prod2);
+			}
+		}
 		return contrib;
 	}
 };
