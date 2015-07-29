24d08c1c177bfdfbac02f0915733d333f1b7e207
Robin Zhang <robinz316@gmail.com>
2015-07-28 17:44:33 -0700

    BK: rename som vars in LMM_g's, stub XOR_g1

--- krypto-lib/src/main/cpp/BridgeKey.h
+++ krypto-lib/src/main/cpp/BridgeKey.h
@@ -52,12 +52,12 @@ public:
 		//untested!
 		PolynomialFunctionTupleChain<N,L> f = _pk.getf();
 
-		BitMatrix<2*N> M2 = _M.inv().split_h_2(1);
-		BitMatrix<2*N> top_mat = _pk.getA().inv() * M2;
-		BitMatrix<2*N> bot_mat = _R.inv() * _pk.getA().inv() * M2;
+		BitMatrix<2*N> M2 = _M.inv().split_v_2(1);
+		BitMatrix<2*N> mat_top = _pk.getA().inv() * M2;
+		BitMatrix<2*N> mat_bot = _R.inv() * _pk.getA().inv() * M2;
 
-		MultivariateQuadraticFunctionTuple<N, N> top = f.get(0) * top_mat;
-		MultivariateQuadraticFunctionTuple<N, N> bot = f.get(0) * bot_mat;
+		MultivariateQuadraticFunctionTuple<N, N> top = f.get(0) * mat_top;
+		MultivariateQuadraticFunctionTuple<N, N> bot = f.get(0) * mat_bot;
 		return (MultivariateQuadraticFunctionTuple<N, 2*N>::aug_v(top, bot)).rMult(_Cu1);
 	}
 
@@ -65,11 +65,11 @@ public:
 		//untested!
 		PolynomialFunctionTupleChain<N,L> f = _pk.getf();
 
-		BitMatrix<2*N> top_mat = _Cu1.inv().split_h_2(0);
-		BitMatrix<2*N> bot_mat = _Cu1.inv().split_h_2(1);
+		BitMatrix<2*N> mat_top = _Cu1.inv().split_v_2(0);
+		BitMatrix<2*N> mat_bot = _Cu1.inv().split_v_2(1);
 
-		MultivariateQuadraticFunctionTuple<N, N> top = f.get(1) * top_mat;
-		MultivariateQuadraticFunctionTuple<N, N> bot = f.get(1) * bot_mat;
+		MultivariateQuadraticFunctionTuple<N, N> top = f.get(1) * mat_top;
+		MultivariateQuadraticFunctionTuple<N, N> bot = f.get(1) * mat_bot;
 		return (MultivariateQuadraticFunctionTuple<N, 2*N>::aug_v(top, bot)).rMult(_Cu2);
 	}
 
@@ -102,10 +102,18 @@ public:
 		return BitMatrix<3*N>::aug_v(Y_top, BitMatrix<3*N>::zeroMatrix(N << 6)) * _Cu2.inv();
 	}
 
-	const BitMatrix<3*N> get_XOR_g1() const{
-		//to be implemented
-		return BitMatrix<3*N>::randomInvertibleMatrix();
-	}
+	// const BitMatrix<3*N> get_XOR_g1() const{
+	// 	//untested!
+	// 	PolynomialFunctionTupleChain<N,L> f = _pk.getf();
+
+	// 	BitMatrix<2*N> M2 = _M.inv().split_v_2(1);
+	// 	BitMatrix<2*N> mat_top = _pk.getA().inv() * M2;
+	// 	BitMatrix<2*N> mat_mid = _pk.getA().inv() * M2;
+
+	// 	MultivariateQuadraticFunctionTuple<N, N> top = f.get(0) * mat_top;
+	// 	MultivariateQuadraticFunctionTuple<N, N> bot = f.get(0) * mat_mid;
+	// 	return (MultivariateQuadraticFunctionTuple<N, 2*N>::aug_v(top, bot)).rMult(_Cu1);
+	// }
 
 	const BitMatrix<3*N> get_XOR_g2() const{
 		//to be implemented
