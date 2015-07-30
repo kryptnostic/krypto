233d1d03123d464f589de7bfd747039fbc4f47b3
howpenghui <phow1@stanford.edu>
2015-07-30 14:16:23 -0700

    merge test_bringe

--- krypto-lib/src/mainTest/cpp/test_bridge.cpp
+++ krypto-lib/src/mainTest/cpp/test_bridge.cpp
@@ -15,21 +15,21 @@ using namespace testing;
 
 #define N 1
 
-// TEST(BridgeKeyTest, testLMM){
-// 	PrivateKey<N, 2> pk;
-// 	BridgeKey<N, 2> bk(pk, BitMatrix<N>::squareIdentityMatrix());
-// 	BitMatrix<4*N> Z = bk.get_LMM_Z();
-// }
+TEST(BridgeKeyTest, testLMM){
+	PrivateKey<N, 2> pk;
+ 	BridgeKey<N, 2> bk(pk, BitMatrix<N>::squareIdentityMatrix());
+ 	BitMatrix<4*N> Z = bk.get_LMM_Z();
+}
 
-// TEST(BridgeKeyTest, testXOR){
-// 	PrivateKey<N, 2> pk;
-// 	BridgeKey<N, 2> bk(pk, BitMatrix<N>::squareIdentityMatrix());
-// 	BitMatrix<2*N> Xx = bk.get_XOR_Xx();
-// 	BitMatrix<2*N> Xy = bk.get_XOR_Xy();
-// }
+TEST(BridgeKeyTest, testXOR){
+ 	PrivateKey<N, 2> pk;
+ 	BridgeKey<N, 2> bk(pk, BitMatrix<N>::squareIdentityMatrix());
+ 	BitMatrix<2*N> Xx = bk.get_XOR_Xx();
+ 	BitMatrix<2*N> Xy = bk.get_XOR_Xy();
+}
 
-// TEST(BridgeKeyTest, testAND){
-// 	PrivateKey<N, 2> pk;
-// 	BridgeKey<N, 2> bk(pk, BitMatrix<N>::squareIdentityMatrix());
-// 	BitMatrix<N> z = bk.get_AND_z();
-// }
+TEST(BridgeKeyTest, testAND){
+ 	PrivateKey<N, 2> pk;
+ 	BridgeKey<N, 2> bk(pk, BitMatrix<N>::squareIdentityMatrix());
+ 	BitMatrix<N> z = bk.get_AND_z();
+}
