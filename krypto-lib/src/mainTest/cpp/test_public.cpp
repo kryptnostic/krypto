328b0a6854a0e5dfd41155f2143ad7f93f2e6e9a
howpenghui <phow1@stanford.edu>
2015-08-03 18:56:55 -0700

    added pubKey test

--- krypto-lib/src/mainTest/cpp/test_public.cpp
+++ krypto-lib/src/mainTest/cpp/test_public.cpp
@@ -16,13 +16,56 @@ using namespace testing;
 
 #define N 1
 
-TEST(PublicKeyTest, testPublicKey){
+TEST(PublicKeyTest, testLMM){
 	PrivateKey<N, 2> pk;
-	BridgeKey<N, 2> bk(pk, BitMatrix<N>::squareIdentityMatrix());
+	BitMatrix<N> K = BitMatrix<N>::randomMatrix(N << 6);
+	BridgeKey<N, 2> bk(pk, K);
 	PublicKey<N, 2> pub(bk);
 
-	// BitVector<N> x; //zero vector
-	// BitVector<2*N> encrypted = pk.encrypt(x);
-	// BitVector<2*N> encryptedLMM = pub.homomorphicLMM(encrypted);
-	// BitVector<N> unencryptedLMM = pk.decrypt(encryptedLMM); //should be zero
+	BitVector<N> x = BitVector<N>::randomVector();
+	BitVector<N> y = BitVector<N>::randomVector();
+	BitVector<2*N> encryptedX = pk.encrypt(x);
+	BitVector<2*N> encryptedY = pk.encrypt(y);
+
+	BitVector<2*N> encryptedLMM = pub.homomorphicLMM(encryptedX);
+	BitVector<N> unencryptedLMM = pk.decrypt(encryptedLMM);
+	BitVector<N> expectedLMM = K.template operator*<N>(x);
+
+	ASSERT_TRUE(expectedLMM.equals(unencryptedLMM));
+}
+
+TEST(PublicKeyTest, testXOR){
+	PrivateKey<N, 2> pk;
+	BitMatrix<N> K = BitMatrix<N>::randomMatrix(N << 6);
+	BridgeKey<N, 2> bk(pk, K);
+	PublicKey<N, 2> pub(bk);
+
+	BitVector<N> x = BitVector<N>::randomVector();
+	BitVector<N> y = BitVector<N>::randomVector();
+	BitVector<2*N> encryptedX = pk.encrypt(x);
+	BitVector<2*N> encryptedY = pk.encrypt(y);
+
+	BitVector<2*N> encryptedXOR = pub.homomorphicXOR(encryptedX, encryptedY);
+	BitVector<N> unencryptedXOR = pk.decrypt(encryptedXOR); 
+	BitVector<N> expectedXOR = x ^ y;
+
+	ASSERT_TRUE(expectedXOR.equals(unencryptedXOR));
+}
+
+TEST(PublicKeyTest, testAND){
+	PrivateKey<N, 2> pk;
+	BitMatrix<N> K = BitMatrix<N>::randomMatrix(N << 6);
+	BridgeKey<N, 2> bk(pk, K);
+	PublicKey<N, 2> pub(bk);
+
+	BitVector<N> x = BitVector<N>::randomVector();
+	BitVector<N> y = BitVector<N>::randomVector();
+	BitVector<2*N> encryptedX = pk.encrypt(x);
+	BitVector<2*N> encryptedY = pk.encrypt(y);
+	
+	BitVector<2*N> encryptedAND = pub.homomorphicAND(encryptedX, encryptedY);
+	BitVector<N> unencryptedAND = pk.decrypt(encryptedAND); 
+	BitVector<N> expectedAND = x & y;
+
+	ASSERT_TRUE(expectedAND.equals(unencryptedAND));	
 }
