35b910d24a51b26d72d7dad66cfdb398217d2c38
howpenghui <phow1@stanford.edu>
2015-08-05 16:38:26 -0700

    BK comment

--- krypto-lib/src/main/cpp/BridgeKey.h
+++ krypto-lib/src/main/cpp/BridgeKey.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Kryptnostic. All rights reserved.
 //
 //  Implementation of bridge key generation
-//	Accessed by clients, used to generate components of the public keys
+//	Accessed by clients, used to generate components of the public keys (Refer to implementation.pdf for the notations)
 //
 
 #ifndef krypto_BridgeKey_h
@@ -18,7 +18,11 @@
 
 using namespace std;
 
-
+/*
+ * Template for BridgeKey
+ * Plaintext Length: 64N, Length of the obfuscation chain: L
+ * L === 2 for now
+ */
 template<unsigned int N, unsigned int L>
 class BridgeKey{
 public:
@@ -28,6 +32,8 @@ public:
 	_Rx(BitMatrix<N>::randomInvertibleMatrix()),
 	_Ry(BitMatrix<N>::randomInvertibleMatrix()),
 	_M(pk.getM()),
+	_Ai(pk.getA().inv()),
+	_Bi(pk.getB().inv()),
 	_Cu1(pk.getUnaryObfChain()[0]),
 	_Cu2(pk.getUnaryObfChain()[1]),
 	_Cb1(pk.getBinaryObfChain()[0]),
@@ -36,16 +42,21 @@ public:
 	_BKBiAi(_BKBi * pk.getA().inv()),
 	_ARAi(pk.getA() * _R * pk.getA().inv()),
 	_ARxAi(pk.getA() * _Rx * pk.getA().inv()),
-	_ARyAi(pk.getA() * _Ry * pk.getA().inv())
+	_ARyAi(pk.getA() * _Ry * pk.getA().inv()),
+	_AiM2(_pk.getA().inv().template pMult<2*N>(_M.inv(), NN, 2*NN-1))
 	{
 	}
 
 /* Unary unified code */
 
+	/*
+	 * Function: get_UNARY_g1
+	 * Returns the first function of the obfuscation chain for unary operations (e.g. left matrix multiplication)
+	 */
 	const MultiQuadTuple<2*N, 2*N> get_UNARY_g1() const{
 		MultiQuadTupleChain<N,L> f = _pk.getf();
 
-		BitMatrix<2*N> mat_top = _pk.getA().inv().template pMult<2*N>(_M.inv(), NN, 2*NN-1);
+		BitMatrix<2*N> mat_top = _AiM2;
 		BitMatrix<2*N> mat_bot = _R * mat_top;
 
 		MultiQuadTuple<2*N, N> top = f.get(0) * mat_top;
@@ -54,6 +65,10 @@ public:
 		return aug.template rMult<2*N>(_Cu1);
 	}
 
+	/*
+	 * Function: get_UNARY_g2
+	 * Returns the second function of the obfuscation chain for unary operations (e.g. left matrix multiplication)
+	 */
 	const MultiQuadTuple<2*N, 2*N> get_UNARY_g2() const{
 		MultiQuadTupleChain<N,L> f = _pk.getf();
 
@@ -68,9 +83,14 @@ public:
 
 /* Left Matrix Multiplication */
 
+	/*
+	 * Function: get_LMM_Z
+	 * Returns matrix Z used for homomorphic left matrix multiplication
+	 * Dimension of Z: 2*(N * 2^6) by 4*(N * 2^6)
+	 */
 	const BitMatrix<4*N> get_LMM_Z() const{
 		BitMatrix<N> zeroN = BitMatrix<N>::squareZeroMatrix();
-		BitMatrix<N> RAi = _R * _pk.getA().inv();
+		BitMatrix<N> RAi = _R * _Ai;
 
 		BitMatrix<2*N> X_top = BitMatrix<2*N>::aug_h(_BKBi, _BKBiAi ^ RAi);
 		BitMatrix<2*N> X_bottom = BitMatrix<2*N>::aug_h(zeroN, _ARAi);
@@ -84,6 +104,10 @@ public:
 
 /* Binary unified code */
 
+	/*
+	 * Function: get_BINARY_g1
+	 * Returns the first function of the obfuscation chain for binary operations (e.g. XOR, AND)
+	 */
 	const MultiQuadTuple<4*N, 3*N> get_BINARY_g1() const{
 		MultiQuadTupleChain<N,L> f = _pk.getf();
 
@@ -91,10 +115,10 @@ public:
 		BitMatrix<4*N> M2_projX = BitMatrix<4*N>::aug_h(M2, BitMatrix<2*N>::zeroMatrix(N << 6));
 		BitMatrix<4*N> M2_projY = BitMatrix<4*N>::aug_h(BitMatrix<2*N>::zeroMatrix(N << 6), M2);
 
-		BitMatrix<4*N> mat_top = _pk.getA().inv() * M2_projX;
-		BitMatrix<4*N> mat_mid = _pk.getA().inv() * M2_projY;
-		BitMatrix<4*N> mat_botX = _Rx * _pk.getA().inv() * M2_projX;
-		BitMatrix<4*N> mat_botY = _Ry * _pk.getA().inv() * M2_projY;
+		BitMatrix<4*N> mat_top = _Ai * M2_projX;
+		BitMatrix<4*N> mat_mid = _Ai * M2_projY;
+		BitMatrix<4*N> mat_botX = _Rx * mat_top;
+		BitMatrix<4*N> mat_botY = _Ry * mat_mid;
 
 		MultiQuadTuple<4*N, N> top = f.get(0) * mat_top;
 		MultiQuadTuple<4*N, N> mid = f.get(0) * mat_mid;
@@ -103,6 +127,10 @@ public:
 		return aug.template rMult<3*N>(_Cb1);
 	}
 
+	/*
+	 * Function: get_BINARY_g2
+	 * Returns the second function (g2) of the obfuscation chain for binary operations (e.g. XOR, AND)
+	 */
 	const MultiQuadTuple<3*N, 3*N>get_BINARY_g2() const{
 		MultiQuadTupleChain<N,L> f = _pk.getf();
 
@@ -121,22 +149,37 @@ public:
 
 /* XOR */
 
+	/*
+	 * Function: get_XOR_Xx
+	 * Returns matrix Xx used for homomorphic XOR
+	 * Dimension of Xx: 2*(N * 2^6) by 2*(N * 2^6)
+	 */
 	const BitMatrix<2*N> get_XOR_Xx() const{
 		BitMatrix<N> idN = BitMatrix<N>::squareIdentityMatrix();
 
-		BitMatrix<2*N> X_top = BitMatrix<2*N>::aug_h(idN, (idN ^ _Rx) * _pk.getA().inv());
+		BitMatrix<2*N> X_top = BitMatrix<2*N>::aug_h(idN, (idN ^ _Rx) * _Ai);
 		BitMatrix<2*N> X_bottom = BitMatrix<2*N>::aug_h(BitMatrix<N>::squareZeroMatrix(), _ARxAi);
 		return _M * BitMatrix<2*N>::aug_v(X_top, X_bottom) * _M.inv();
 	}
 
+	/*
+	 * Function: get_XOR_Xy
+	 * Returns matrix Xy used for homomorphic XOR
+	 * Dimension of Xy: 2*(N * 2^6) by 2*(N * 2^6)
+	 */
 	const BitMatrix<2*N> get_XOR_Xy() const{
 		BitMatrix<N> idN = BitMatrix<N>::squareIdentityMatrix();
 
-		BitMatrix<2*N> X_top = BitMatrix<2*N>::aug_h(idN, (idN ^ _Ry) * _pk.getA().inv());
+		BitMatrix<2*N> X_top = BitMatrix<2*N>::aug_h(idN, (idN ^ _Ry) * _Ai);
 		BitMatrix<2*N> X_bottom = BitMatrix<2*N>::aug_h(BitMatrix<N>::squareZeroMatrix(), _ARyAi);
 		return _M * BitMatrix<2*N>::aug_v(X_top, X_bottom) * _M.inv();
 	}
 
+	/*
+	 * Function: get_XOR_Y
+	 * Returns matrix Y used for homomorphic XOR
+	 * Dimension of Y: 2*(N * 2^6) by 3*(N * 2^6)
+	 */
 	const BitMatrix<3*N> get_XOR_Y() const{
 		BitMatrix<N> idN = BitMatrix<N>::squareIdentityMatrix();
 
@@ -146,6 +189,10 @@ public:
 
 /* AND */
 
+	/*
+	 * Function: get_AND_z
+	 * Returns function tuple z used for homomorphic AND
+	 */
 	const MultiQuadTuple<7*N, 2*N> get_AND_z() const{
 		BitMatrix<2*N> X = get_AND_X();
 		BitMatrix<3*N> Y1 = get_AND_Y1();
@@ -153,17 +200,15 @@ public:
 		BitMatrix<3*N> Y3 = get_AND_Y3();
 		BitMatrix<7*N> Y3t = BitMatrix<7*N>::aug_h(BitMatrix<4*N>::zeroMatrix(N << 6), Y3);
 
-		//maybe it was because X,X was not added
-
 		BitMatrix<N> contrib = get_AND_Pk(0, X, Y2);
 
-		for (int level = 1; level < twoNN; ++level) {
+		for (size_t level = 1; level < twoNN; ++level) {
 			contrib = BitMatrix<N>::aug_v(contrib, get_AND_Pk(level, X, Y2)); //add P_k's
 		}
-		for (int level = 0; level < twoNN; ++level) {
+		for (size_t level = 0; level < twoNN; ++level) {
 			contrib = BitMatrix<N>::aug_v(contrib, get_AND_Qk(level, X, Y1)); //add Q_k's
 		}
-		for (int level = 0; level < threeNN; ++level) {
+		for (size_t level = 0; level < threeNN; ++level) {
 			contrib = BitMatrix<N>::aug_v(contrib, get_AND_Sk(level, Y1, Y2)); //add S_k's
 		}
 
@@ -177,14 +222,24 @@ public:
 		return result;
 	}
 
+	/*
+	 * Function: get_AND_Z1
+	 * Returns matrix Z1 used for homomorphic AND
+	 * Dimension of Z1: 2*(N * 2^6) by 2*(N * 2^6) 
+	 */
 	const BitMatrix<2*N> get_AND_Z1() const{
-		BitMatrix<2*N> top = _Rx * _pk.getA().inv().template pMult<2*N>(_M.inv(), NN, 2*NN-1);// * M2;
+		BitMatrix<2*N> top = _Rx * _AiM2;
 		BitMatrix<2*N> bottom = _pk.getA() * top;
 		return _M * BitMatrix<2*N>::aug_v(top, bottom);
 	}
 
+	/*
+	 * Function: get_AND_Z2
+	 * Returns matrix Z2 used for homomorphic AND
+	 * Dimension of Z2: 2*(N * 2^6) by 2*(N * 2^6) 
+	 */
 	const BitMatrix<2*N> get_AND_Z2() const{
-		BitMatrix<2*N> top = _Ry * _pk.getA().inv().template pMult<2*N>(_M.inv(), NN, 2*NN-1);// * M2;
+		BitMatrix<2*N> top = _Ry * _AiM2;
 		BitMatrix<2*N> bottom = _pk.getA() * top;
 		return _M * BitMatrix<2*N>::aug_v(top, bottom);
 	}
@@ -207,39 +262,65 @@ private:
 	BitMatrix<N> _ARyAi;
 	MultiQuadTupleChain<2*N,L> _g_u; //obsfucated chain for unary operations
 	MultiQuadTupleChain<3*N,L> _g_b; //obsfucated chain for binary operations
-	int _dim_quad = 64; //dimension of bitmatrix used to represent quadratic poly's (why isn't this N << 6 in general?)
+	
+	BitMatrix<N> _Ai;
+	BitMatrix<N> _Bi;
+	BitMatrix<2*N> _AiM2;
 	static const unsigned int twoN = N << 1;
 	static const unsigned int threeN = 3 * N;
 	static const unsigned int NN = N << 6;
 	static const unsigned int twoNN = NN << 1;
 	static const unsigned int threeNN = 3 * NN;
 
-	//can be combined into get_z
+/*Helper functions for get_AND_z*/
+
+	/*
+	 * Function: get_AND_X
+	 * Returns matrix X used to compute z for homomorphic AND
+	 * Dimension of X: (N * 2^6) by 2*(N * 2^6)
+	 */
 	const BitMatrix<2*N> get_AND_X() const{
-		BitMatrix<2*N> inner = BitMatrix<2*N>::aug_h(BitMatrix<N>::squareIdentityMatrix(), _pk.getA().inv());
-		return _pk.getB().inv() * inner * _M.inv();
+		BitMatrix<2*N> inner = BitMatrix<2*N>::aug_h(BitMatrix<N>::squareIdentityMatrix(), _Ai);
+		return _Bi * inner * _M.inv();
 	}
 
+	/*
+	 * Function: get_AND_Y1
+	 * Returns matrix Y1 used to compute z for homomorphic AND
+	 * Dimension of X: (N * 2^6) by 3*(N * 2^6)
+	 */
 	const BitMatrix<3*N> get_AND_Y1() const{
-		return _pk.getB().inv().pMult(_Cb2.inv(), 0, NN-1);
+		return _Bi.pMult(_Cb2.inv(), 0, NN-1);
 	}
 
+	/*
+	 * Function: get_AND_Y2
+	 * Returns matrix Y2 used to compute z for homomorphic AND
+	 * Dimension of X: (N * 2^6) by 3*(N * 2^6)
+	 */
 	const BitMatrix<3*N> get_AND_Y2() const{
-		return _pk.getB().inv().pMult(_Cb2.inv(), NN, 2*NN-1);
+		return _Bi.pMult(_Cb2.inv(), NN, 2*NN-1);
 	}
 
+	/*
+	 * Function: get_AND_Y3
+	 * Returns matrix Y3 used to compute z for homomorphic AND
+	 * Dimension of X: (N * 2^6) by 3*(N * 2^6)
+	 */
 	const BitMatrix<3*N> get_AND_Y3() const{
 		return _Cb2.inv().split_v_3(2);
 	}
 
-	//top chunk of contrib matrix for z
-	//level ranges from 0 to 64 * 2N - 1
+	/* 
+	 * Function: get_AND_Pk
+	 * top chunk of contrib matrix for z for homomorphic AND
+	 * level ranges from 0 to 64 * 2N - 1
+	 */
 	const BitMatrix<N> get_AND_Pk(const int level, const BitMatrix<2*N> &X, const BitMatrix<3*N> &Y2) const{
-		//untested!
 		BitMatrix<N> top = BitMatrix<N>::zeroMatrix(twoNN - level); //not sure if there's obob here
 
 		BitMatrix<N> mid = BitMatrix<N>::zeroMatrix(twoNN);
-		for (int j = 0; j < NN; ++j) { //col within middle block
+		for (size_t j = 0; j < NN; ++j) { //col within middle block
 			bool lhs = X.get(j, level);
 			for (int i = 0; i < twoNN; ++i) { //row within middle block
 				bool rhs = X.get(j, i);
@@ -248,7 +329,7 @@ private:
 		}
 
 		BitMatrix<N> bot = BitMatrix<N>::zeroMatrix(threeNN);
-		for (int i = 0; i < threeNN; ++i) { //row within bottom block
+		for (size_t i = 0; i < threeNN; ++i) { //row within bottom block
 			for (int j = 0; j < NN; ++j) { //col within bottom block
 				bool lhs = X.get(j, level);
 				bool rhs = Y2.get(j, i);
@@ -258,14 +339,17 @@ private:
 		return BitMatrix<N>::aug_v(BitMatrix<N>::aug_v(top, mid), bot);
 	}
 
-	//middle chunk of contrib matrix for z
-	//level ranges from 0 to 64 * 2N - 1
+	/*
+	 * Function: get_AND_Qk
+	 * middle chunk of contrib matrix for z
+	 * level ranges from 0 to 64 * 2N - 1
+	 */
 	const BitMatrix<N> get_AND_Qk(const int level, const BitMatrix<2*N>&X, const BitMatrix<3*N> &Y1) const{
 		BitMatrix<N> top = BitMatrix<N>::zeroMatrix(twoNN - level);
 
 		BitMatrix<N> bot = BitMatrix<N>::zeroMatrix(threeNN);
-		for (int i = 0; i < threeNN; ++i) { //row within bottom block
-			for (int j = 0; j < NN; ++j) { //col within bottom block
+		for (size_t i = 0; i < threeNN; ++i) { //row within bottom block
+			for (size_t j = 0; j < NN; ++j) { //col within bottom block
 				bool lhs = X.get(j, level);
 				bool rhs = Y1.get(j, i);
 				bot.set(i, j, lhs && rhs);
@@ -274,15 +358,18 @@ private:
 		return BitMatrix<N>::aug_v(top, bot);
 	}
 
-	//bottom chunk of contrib matrix for z
-	//level ranges from 0 to 64 * 3N - 1
+	/*
+	 * Function: get_AND_Sk
+	 * bottom chunk of contrib matrix for z
+	 * level ranges from 0 to 64 * 3N - 1
+	 */
 	const BitMatrix<N> get_AND_Sk(const int level, const BitMatrix<3*N> &Y1, const BitMatrix<3*N> &Y2) const{
 		BitMatrix<N> contrib = BitMatrix<N>::zeroMatrix(threeNN - level);
-		for (int j = 0; j < NN; ++j) { //cols
+		for (size_t j = 0; j < NN; ++j) { //cols
 			bool prod = Y1.get(j, level) && Y2.get(j, level); //first row
 			contrib.set(0, j, prod);
 
-			for (int i = 1; i < threeNN - level; ++i) { //rows
+			for (size_t i = 1; i < threeNN - level; ++i) { //rows
 				bool prod1 = Y1.get(j, level) && Y2.get(j, level + i);
 				bool prod2 = Y1.get(j, level + i) && Y2.get(j, level);
 				contrib.set(i, j, prod1 ^ prod2);
