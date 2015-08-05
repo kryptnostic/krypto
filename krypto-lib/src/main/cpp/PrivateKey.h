f38abf548ce99644aaa0af2b3098a3e2b1e33492
howpenghui <phow1@stanford.edu>
2015-08-05 14:10:46 -0700

    minor change

--- krypto-lib/src/main/cpp/PrivateKey.h
+++ krypto-lib/src/main/cpp/PrivateKey.h
@@ -72,7 +72,7 @@ private:
 	vector<BitMatrix<2*N> > _C_u; //chain of obfuscation matrix for unary operations
 	vector<BitMatrix<3*N> > _C_b; //chain of obfuscation matrix for binary operations
 	void generateObfuscationMatrixChains(){ //generates C_{u1},...,C_{uL} and C_{b1},...,C_{bL}
-		for(int i = 0; i < L; ++i){
+		for(size_t i = 0; i < L; ++i){
 			_C_u.push_back(BitMatrix<(2*N)>::randomInvertibleMatrix());
 			_C_b.push_back(BitMatrix<(3*N)>::randomInvertibleMatrix());
 		}
