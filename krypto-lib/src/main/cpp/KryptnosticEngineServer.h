9146ee333d00c636b2bdfb5d78a728a87c9765f3
Robin Zhang <robinz316@gmail.com>
2015-08-21 14:33:24 -0700

    KES: implement getMetadataAddress, CHF: define N

--- krypto-lib/src/main/cpp/KryptnosticEngineServer.h
+++ krypto-lib/src/main/cpp/KryptnosticEngineServer.h
@@ -31,7 +31,7 @@ public:
      * Constructor
      * Constructs a Kryptnostic Engine given private and public keys
      */
-	KryptnosticEngineServer(const ClientHashFunction cHashFunction, const BitVector<N> eDocSearchKey, const BitMatrix<N, N> docConversionMatrix) :
+	KryptnosticEngineServer(const ClientHashFunction cHashFunction, const BitVector<2*N> eDocSearchKey, const BitMatrix<N, N> docConversionMatrix) :
 	_cHashFunction(cHashFunction),
 	_eDocSearchKey(eDocSearchKey),
 	_docConversionMatrix(docConversionMatrix)
@@ -46,9 +46,11 @@ public:
 	 * Function: getMetadataAddress
 	 * Returns a serialized pair of (DocumentSearchKey, DocumentAddressFunction)
 	 */
-	const BitVector<N> getMetadataAddress(const BitVector<N> eSearchToken) const{
-
-		return 0;
+	const BitVector<N> getMetadataAddress(const BitVector<2*N> eSearchToken) const{
+		BitVector<N> hashMatrixOutput = _cHashFunction.hashMatrix * BitVector<4*N>::vCat(eSearchToken, _eDocSearchKey);
+		BitVector<2*N> augmentedOutputF1 = BitVector<2*N>::vCat(_cHashFunction.concealedF1(eSearchToken), _cHashFunction.concealedF1(_eDocSearchKey));
+		BitVector<N> functionalOutput = _cHashFunction.augmentedF2(augmentedOutputF1);
+		return hashMatrixOutput ^ functionalOutput;
 	}
 
 
