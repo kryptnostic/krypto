aa3f56c5ecda398243562fe542296855998c2566
HPH <phow1@stanford.edu>
2015-08-25 16:27:26 -0700

    Add search test

--- krypto-lib/src/mainTest/cpp/test_search.cpp
+++ krypto-lib/src/mainTest/cpp/test_search.cpp
@@ -12,10 +12,20 @@ using namespace testing;
 
 #define N 64
 
-TEST(SearchKeyTest, testCalls){
+TEST(SearchKeyTest, testCallsWithoutPrivateKey){
 	SearchPrivateKey<N> sk;
-	BitMatrix<N, 2*N> K = sk.getK();
 	BitVector<N> objectSearchKey = sk.getObjectSearchKey();
 	BitMatrix<N, 2*N> objectAddressFunction = sk.getObjectAddressFunction();
 	BitMatrix<N> objectConversionMatrix = sk.getObjectConversionMatrix(objectAddressFunction);
+}
+
+TEST(SearhKeyTest, testClientHashFunction){
+	SearchPrivateKey<N> sk;
+	PrivateKey<N> pk;
+	ClientHashFunction<N> chf = sk.getClientHashFunction(pk);
+	BitVector<N> searchToken = BitVector<N>::randomVector();
+	BitVector<2*N> eSearchToken = pk.encrypt(searchToken);
+	BitVector<N> objectSearchKey = sk.getObjectSearchKey();
+	BitVector<2*N> eObjectSearchKey = pk.encrypt(objectSearchKey);
+	BitVector<N> metadatumAddress = chf(eSearchToken, eObjectSearchKey);
 }
