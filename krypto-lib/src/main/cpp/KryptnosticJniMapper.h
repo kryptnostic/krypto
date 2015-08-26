0719a02a15386089a0035517c1f198648c862029
Drew <drew@kryptnostic.com>
2015-08-26 00:30:44 -0700

    still need to implement bitvector to jbyteArray and destruction

--- krypto-lib/src/main/cpp/KryptnosticJniMapper.h
+++ krypto-lib/src/main/cpp/KryptnosticJniMapper.h
@@ -17,35 +17,54 @@ const BitVector<2*N> * convertJByteArrayToBitVector( JNIEnv * env, jbyteArray co
 	return reinterpret_cast<BitVector<2*N>*>( buffPtr );
 }
 
+const jbyteArray convertBitVectorToJByteArray( JNIEnv * env, BitVector<N> bitVect ) {
+	return NULL;
+}
+
 const ClientHashFunction<N> * convertJByteArrayToClientHashFunction( JNIEnv * env, jbyteArray convertMe ) {
 	jbyte* buffPtr = env->GetByteArrayElements( convertMe, NULL );
 	return reinterpret_cast<ClientHashFunction<N>*>( buffPtr );
 }
 
-void destroy( JNIEnv * env, jbyte * killMe, jbyteArray original ) {
-	env->ReleaseByteArrayElements( original, killMe, 0) ;
+const jfieldID getServerHandleField( JNIEnv *env, jobject javaContainer ) {
+	jclass c = env->GetObjectClass( javaContainer );
+	jfieldID field = env->GetFieldID( c, "handle", "J" );
+	return field;
 }
 
-void Java_com_kryptnostic_krypto_engine_KryptnosticEngine_initKryptnosticService( JNIEnv * environ, jobject javaContainer, jbyteArray hash, jbyteArray encSearchToken ) {
-	printf("Initing Kryptnostic Service calledddd \n");
-	fflush(stdout);
+template <typename T>
+T *getKryptnosticServer( JNIEnv *env, jobject javaContainer ) {
+	jlong handle = env->GetLongField( javaContainer, getServerHandleField( env, javaContainer ) );
+	return reinterpret_cast<T *>( handle );
+}
 
-	const BitVector<2*N> * eSearchToken = convertJByteArrayToBitVector( environ, encSearchToken );
-	const ClientHashFunction<N> * clientHashFunction = convertJByteArrayToClientHashFunction( environ, encSearchToken );
+template <typename T>
+void setKryptnosticServer( JNIEnv *env, jobject javaContainer, T *t) {
+	jlong serverAddress = reinterpret_cast<jlong>( t );
+	env->SetLongField( javaContainer, getServerHandleField( env, javaContainer ), serverAddress );
+}
+
+void Java_com_kryptnostic_krypto_engine_KryptnosticEngine_initKryptnosticService( JNIEnv * environment, jobject javaContainer, jbyteArray hash, jbyteArray encSearchToken ) {
+	const BitVector<2*N> * eSearchToken = convertJByteArrayToBitVector( environment, encSearchToken );
+	const ClientHashFunction<N> * clientHashFunction = convertJByteArrayToClientHashFunction( environment, encSearchToken );
 	KryptnosticServer serv = KryptnosticServer::KryptnosticServer( *clientHashFunction, *eSearchToken );
-
 	// set the long in java
-	jclass c = environ->GetObjectClass( javaContainer );
-	jfieldID field = environ->GetFieldID( c, "handle", "J" );
-	jlong serverPtr = (jlong)&serv;
-	environ->SetLongField( javaContainer, field, serverPtr );
-	//return serverPtr;
+	setKryptnosticServer( environment, javaContainer, &serv );
 }
 
-jbyteArray Java_com_kryptnostic_krypto_engine_KryptnosticEngine_calculateMetadataAddress( JNIEnv * environ, jobject, jbyteArray searchKey, jbyteArray conversionMatrix ) {
-	printf("Calculate Metadata address calledddd \n");
-	const BitMatrix<N, N> * objectConvMatrix = convertJByteArrayToBitMatrix( environ, conversionMatrix );
-	const BitVector<2*N> * encObjectSearchKey = convertJByteArrayToBitVector( environ, searchKey );
+jbyteArray Java_com_kryptnostic_krypto_engine_KryptnosticEngine_calculateMetadataAddress( JNIEnv * environment, jobject javaContainer, jbyteArray searchKey, jbyteArray conversionMatrix ) {
+	const BitMatrix<N, N> * objectConvMatrix = convertJByteArrayToBitMatrix( environment, conversionMatrix );
+	const BitVector<2*N> * encObjectSearchKey = convertJByteArrayToBitVector( environment, searchKey );
+	KryptnosticServer serv = *getKryptnosticServer<KryptnosticServer>( environment, javaContainer );
+	BitVector<N> metadataAddress = serv.getMetadataAddress( *encObjectSearchKey, *objectConvMatrix );
+	jbyteArray finalRay = convertBitVectorToJByteArray( environment, metadataAddress );
+	return finalRay;
 }
+
+// destroy() {
+//      KryptnosticServer *serv = getKryptnosticServer( environment, javaContainer );
+//      setKryptnosticServer( environment, javaContainer, 0 );
+//      delete serv;
+//}
 
 #endif
