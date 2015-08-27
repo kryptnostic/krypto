050e46f39b2e33788f71a8710024634afdf043be
HPH <phow1@stanford.edu>
2015-08-26 18:29:54 -0700

    Search test updated, need to fix chf

--- krypto-lib/src/mainTest/cpp/test_search.cpp
+++ krypto-lib/src/mainTest/cpp/test_search.cpp
@@ -12,20 +12,44 @@ using namespace testing;
 
 #define N 64
 
-TEST(SearchKeyTest, testCallsWithoutPrivateKey){
+TEST(SearchKeyTest, testClientHashFunctionCall){
 	SearchPrivateKey<N> sk;
-	BitVector<N> objectSearchKey = sk.getObjectSearchKey();
-	//BitMatrix<N, 2*N> objectAddressFunction = sk.getObjectAddressFunction();
-	//BitMatrix<N> objectConversionMatrix = sk.getObjectConversionMatrix(objectAddressFunction);
+	PrivateKey<N> pk;
+	ClientHashFunction<N> chf = sk.getClientHashFunction(pk);
 }
 
-TEST(SearchKeyTest, testIndexing){
+TEST(SearchKeyTest, testIndexingAndSearch){
 	SearchPrivateKey<N> sk;
 	PrivateKey<N> pk;
 
-	BitVector<N> token = BitVector<N>::randomVector();
-
 	BitVector<N> objectSearchKey = sk.getObjectSearchKey();
 	BitMatrix<N> objectAddressFunction = sk.getObjectAddressFunction();
-	//std::pair<BitVector<2*N>, BitMatrix<N> > objectIndexPair = sk.getObjectIndexPair(pk);
+
+	std::pair<BitVector<2*N>, BitMatrix<N> > objectIndexPair = sk.getObjectIndexPair(objectSearchKey, objectAddressFunction, pk);
+
+	BitVector<N> token = BitVector<N>::randomVector();
+
+	BitVector<N> metadatumAddress = sk.getMetadatumAddress(objectAddressFunction, token, objectSearchKey);
+
+	BitVector<2*N> eToken = pk.encrypt(token);
+	BitVector<2*N> eObjectSearchKey = pk.encrypt(objectSearchKey);
+
+	BitMatrix<N> objectConversionMatrix = sk.getObjectConversionMatrix(objectAddressFunction);
+	ClientHashFunction<N> chf = sk.getClientHashFunction(pk);
+
+	BitVector<N> calculatedAddress = objectConversionMatrix * chf(eToken, eObjectSearchKey);
+
+	metadatumAddress.print();
+	calculatedAddress.print();
+
+	//TODO: assert
+}
+
+TEST(SearchKeyTest, testSharing){
+	SearchPrivateKey<N> sk_src;
+	SearchPrivateKey<N> sk_dst;
+	PrivateKey<N> pk_src;
+	PrivateKey<N> pk_dst;
+
+	//TODO: test sharing
 }
