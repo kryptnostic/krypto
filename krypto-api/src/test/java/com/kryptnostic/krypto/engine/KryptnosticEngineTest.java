b8551a9fb2fad4d6d27f02abf50e06ae063732d7
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2015-09-08 21:22:50 -0700

    Added test for two clients sharing in Java.

--- krypto-api/src/test/java/com/kryptnostic/krypto/engine/KryptnosticEngineTest.java
+++ krypto-api/src/test/java/com/kryptnostic/krypto/engine/KryptnosticEngineTest.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -13,83 +14,121 @@ import static org.junit.Assert.assertArrayEquals;
 
 public class KryptnosticEngineTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {}
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @Before
+    public void setUp() throws Exception {}
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception {}
 
-	@Test
-	public void testBitVectorConversions() throws Exception {
-		Random rand = new Random();
-		byte[] vector = new byte[16];
-		rand.nextBytes(vector);
-		byte[] output = KryptnosticEngine.testBitVectorConversion(vector);
-		assertArrayEquals(vector, output);
-	}
+    @Test
+    public void testSearchFlow() {
+        KryptnosticEngine clientEngine = new KryptnosticEngine();
+        KryptnosticEngine clientEngine2 = new KryptnosticEngine();
+        
+        clientEngine.initClient();
+        clientEngine2.initClient();
+        
+        Random r = new Random();
 
-	@Test
-	public void testBitMatrixConversions() throws Exception {
-		Random rand = new Random();
-		byte[] matrix = new byte[2048];
-		rand.nextBytes(matrix);
-		byte[] output = KryptnosticEngine.testBitMatrixConversion(matrix);
-		assertArrayEquals(matrix, output);
-	}
+        byte[] searchToken = new byte[ 16 ];
 
-	@Test
-	public void testClientHashFunctionConversions() throws Exception {
-		Random rand = new Random();
-		byte[] chf = new byte[1060896];
-		rand.nextBytes(chf);
-		byte[] output = KryptnosticEngine.testClientHashFunctionConversion(chf);
-		assertArrayEquals(chf, output);
-	}
+        r.nextBytes( searchToken );
 
-	@Test
-	public void testMetadataAddressCalculations() throws Exception {
-		Random rand = new Random();
-		byte[] pk = KryptnosticEngine.testPrivateKey();
-		byte[] spk = KryptnosticEngine.testSearchPrivateKey();
-		byte[] osk = KryptnosticEngine.testObjectSearchKey(spk);
-		byte[] oam = KryptnosticEngine.testObjectAddressMatrix(spk);
-		byte[] ocm = KryptnosticEngine.testObjectConversionMatrix(spk, oam);
-		byte[] token = new byte[16];
-		rand.nextBytes(token);
-		byte[] clientCalculatedAddress = KryptnosticEngine.testClientMetadataAddress(spk, oam, osk, token);
+        byte[] encryptedSearchToken = clientEngine.getEncryptedSearchToken( searchToken );
 
-		byte[] etoken = KryptnosticEngine.testEncryptionFHE(pk, token);
-		byte[] eosk = KryptnosticEngine.testEncryptionFHE(pk, osk);
-		byte[] chf = KryptnosticEngine.testClientHashFunction(spk, pk);
-		byte[] objectIndexPair = ByteBuffer.allocate( eosk.length + ocm.length ).put( eosk ).put( ocm ).array();
+        KryptnosticEngine serverEngine = new KryptnosticEngine(
+                clientEngine.getClientHashFunction(),
+                encryptedSearchToken );
+        KryptnosticEngine serverEngine2 = new KryptnosticEngine(
+                clientEngine2.getClientHashFunction(),
+                encryptedSearchToken );
 
-		KryptnosticEngine ke = new KryptnosticEngine(chf, etoken);
-		byte[] serverCalculatedAddress = ke.calculateMetadataAddress( objectIndexPair );
+        byte[] objectAddressMatrix = clientEngine.getObjectAddressMatrix();
+        byte[] objectSearchKey = clientEngine.getObjectSearchKey();
 
-//		System.out.print("Client-calculated = [");
-//		for( byte b : clientCalculatedAddress ){
-//			System.out.print( (int) b );
-//			System.out.print(",");
-//		}
-//		System.out.println("]");
-//
-//		System.out.print("Server-calculated = [");
-//		for( byte b : serverCalculatedAddress ){
-//			System.out.print( (int) b );
-//			System.out.print(",");
-//		}
-//		System.out.println("]");
+        byte[] address = clientEngine.clientGetMetadatumAddress( objectAddressMatrix, objectSearchKey, searchToken );
 
-		assertArrayEquals(clientCalculatedAddress, serverCalculatedAddress);
-	}
+        byte[] objectIndexPair = clientEngine.getObjectIndexPair( objectSearchKey, objectAddressMatrix );
+
+        // Tests sharing
+        byte[] sharingPair = clientEngine.getObjectSharingPair( clientEngine.getObjectIndexPair( objectSearchKey,
+                objectAddressMatrix ) );
+        byte[] indexPairFromSharingPair = clientEngine2.getObjectIndexPairFromSharing( sharingPair );
+
+        byte[] serverAddressDirect = serverEngine.calculateMetadataAddress( objectIndexPair );
+        byte[] serverAddressFromSharing = serverEngine2.calculateMetadataAddress( indexPairFromSharingPair );
+        Assert.assertArrayEquals( address, serverAddressDirect );
+        Assert.assertArrayEquals( address, serverAddressFromSharing );
+        Assert.assertArrayEquals( serverAddressDirect, serverAddressFromSharing );
+    }
+
+    @Test
+    public void testBitVectorConversions() throws Exception {
+        Random rand = new Random();
+        byte[] vector = new byte[ 16 ];
+        rand.nextBytes( vector );
+        byte[] output = KryptnosticEngine.testBitVectorConversion( vector );
+        assertArrayEquals( vector, output );
+    }
+
+    @Test
+    public void testBitMatrixConversions() throws Exception {
+        Random rand = new Random();
+        byte[] matrix = new byte[ 2048 ];
+        rand.nextBytes( matrix );
+        byte[] output = KryptnosticEngine.testBitMatrixConversion( matrix );
+        assertArrayEquals( matrix, output );
+    }
+
+    @Test
+    public void testClientHashFunctionConversions() throws Exception {
+        Random rand = new Random();
+        byte[] chf = new byte[ 1060896 ];
+        rand.nextBytes( chf );
+        byte[] output = KryptnosticEngine.testClientHashFunctionConversion( chf );
+        assertArrayEquals( chf, output );
+    }
+
+    @Test
+    public void testMetadataAddressCalculations() throws Exception {
+        Random rand = new Random();
+        byte[] pk = KryptnosticEngine.testPrivateKey();
+        byte[] spk = KryptnosticEngine.testSearchPrivateKey();
+        byte[] osk = KryptnosticEngine.testObjectSearchKey( spk );
+        byte[] oam = KryptnosticEngine.testObjectAddressMatrix( spk );
+        byte[] ocm = KryptnosticEngine.testObjectConversionMatrix( spk, oam );
+        byte[] token = new byte[ 16 ];
+        rand.nextBytes( token );
+        byte[] clientCalculatedAddress = KryptnosticEngine.testClientMetadataAddress( spk, oam, osk, token );
+
+        byte[] etoken = KryptnosticEngine.testEncryptionFHE( pk, token );
+        byte[] eosk = KryptnosticEngine.testEncryptionFHE( pk, osk );
+        byte[] chf = KryptnosticEngine.testClientHashFunction( spk, pk );
+        byte[] objectIndexPair = ByteBuffer.allocate( eosk.length + ocm.length ).put( eosk ).put( ocm ).array();
+
+        KryptnosticEngine ke = new KryptnosticEngine( chf, etoken );
+        byte[] serverCalculatedAddress = ke.calculateMetadataAddress( objectIndexPair );
+
+        // System.out.print("Client-calculated = [");
+        // for( byte b : clientCalculatedAddress ){
+        // System.out.print( (int) b );
+        // System.out.print(",");
+        // }
+        // System.out.println("]");
+        //
+        // System.out.print("Server-calculated = [");
+        // for( byte b : serverCalculatedAddress ){
+        // System.out.print( (int) b );
+        // System.out.print(",");
+        // }
+        // System.out.println("]");
+
+        assertArrayEquals( clientCalculatedAddress, serverCalculatedAddress );
+    }
 }
