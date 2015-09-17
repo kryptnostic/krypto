20fbf83d94942afe4c87872a454dd69af54c07be
Robin Zhang <robinz316@gmail.com>
2015-09-17 11:51:49 -0700

    sizeof: update pair naming

--- krypto-lib/src/main/js/sizeof.cpp
+++ krypto-lib/src/main/js/sizeof.cpp
@@ -14,41 +14,32 @@ using namespace std;
 #define N 128
 
 int main(){
-	cout << "N: " << N << endl;
-
-	cout << endl;
-
-	cout << "PrivateKey<N>: " << sizeof(PrivateKey<N>) << endl;
-	cout << "BridgeKey<N>: " << sizeof(BridgeKey<N>) << endl;
-	cout << "PublicKey<N>: " << sizeof(PublicKey<N>) << endl;
-	cout << "SearchPrivateKey<N>: " << sizeof(SearchPrivateKey<N>) << endl;
-	cout << "MultiQuadTuple<N,N>: " << sizeof(MultiQuadTuple<N, N>) << endl;
-	cout << "MultiQuadTuple<2*N,N>: " << sizeof(MultiQuadTuple<2*N, N>) << endl;
-	cout << "MultiQuadTuple<N,2*N>: " << sizeof(MultiQuadTuple<N, 2*N>) << endl;
+	cout << "Search Token: " << N << endl;
 
 	cout << endl;
 	cout << "Registration: " << endl;
 
-	cout << "getPrivateKey: " << sizeof(PrivateKey<N>) << endl;
-	cout << "getSearchPrivateKey: " << sizeof(SearchPrivateKey<N>) << endl;
+	cout << "PrivateKey: " << sizeof(PrivateKey<N>) << endl;
+	cout << "SearchPrivateKey: " << sizeof(SearchPrivateKey<N>) << endl;
 	cout << "getClientHashFunction: " << sizeof(ClientHashFunction<N>) << endl;
 
 	cout << endl;
 	cout << "Indexing: " << endl;
 
-	cout << "getMetadatumAddress: " << sizeof(BitVector<N>) << endl;
-	cout << "getObjectAddressFunction: " << sizeof(BitMatrix<N, 2*N>) << endl;
-	cout << "getObjectIndexPair: " << sizeof(std::pair <BitVector<2*N>,BitMatrix<N> >) << endl;
+	cout << "MetadataAddress: " << sizeof(BitVector<N>) << endl;
+	cout << "ObjectAddressMatrix: " << sizeof(BitMatrix<N, 2*N>) << endl;
+	cout << "ObjectIndexPair: " << sizeof(std::pair <BitVector<N>,BitMatrix<N> >) << endl;
 
 	cout << endl;
 	cout << "Search: " << endl;
 
-	cout << "getEncryptedSearchToken: " << sizeof(BitVector<2*N>) << endl;
+	cout << "ObjectSearchPair: " << sizeof(std::pair <BitVector<2*N>,BitMatrix<N> >) << endl;
+	cout << "EncryptedSearchToken: " << sizeof(BitVector<2*N>) << endl;
 
 	cout << endl;
 	cout << "Sharing: " << endl;
 
-	cout << "getObjectSharingPair: " << sizeof(std::pair <BitVector<2*N>,BitMatrix<N> >) << endl;
+	cout << "ObjectSharePair: " << sizeof(std::pair <BitVector<N>,BitMatrix<N> >) << endl;
 
 	return 0;
 }
