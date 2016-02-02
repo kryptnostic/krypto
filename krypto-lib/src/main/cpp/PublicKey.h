87b1f9e7da6cd4c67bfae9835c26b4b4aac3cfb6
Nicholas Lai <nickjhlai@gmail.com>
2016-02-02 10:18:30 -0800

    Random Rx, Ry
    Instantiate Rx, Ry randomly when needed and modify ARxAi. ARyAi
    accordingly.

--- krypto-lib/src/main/cpp/PublicKey.h
+++ krypto-lib/src/main/cpp/PublicKey.h
@@ -26,8 +26,6 @@ public:
 	_lc(bk.getLeftColumnMatrix()),
 	_gu1(bk.getUnaryG1()),
 	_gu2(bk.getUnaryG2()),
-	_gb1(bk.getBinaryG1()),
-	_gb2(bk.getBinaryG2()),
 	_XOR(bk.getXOR()),
 	_AND(bk.getAND())
 	{
@@ -38,11 +36,11 @@ public:
 	}
 
 	const BitVector<2*N> homomorphicXOR(const BitVector<2*N> &x, const BitVector<2*N> &y) const{
-		return _XOR(x, y, binaryT(x, y));
+		return _XOR(x, y);
 	}
 
 	const BitVector<2*N> homomorphicAND(const BitVector<2*N> &x, const BitVector<2*N> &y) const{
-		return _AND(x, y, binaryT(x, y));
+		return _AND(x, y);
 	}
 
 	//single left shift (if the leftmost bit of x is nonzero, it'll be zeroed)
@@ -88,15 +86,8 @@ private:
 	const BitMatrix<2*N, 4*N> _lc;
 	const MultiQuadTuple<2*N, 2*N> _gu1;
 	const MultiQuadTuple<2*N, 2*N> _gu2;
-	const MultiQuadTuple<4*N, 3*N> _gb1;
-	const MultiQuadTuple<3*N, 3*N> _gb2;
 	const typename BridgeKey<N>::H_XOR _XOR;
 	const typename BridgeKey<N>::H_AND _AND;
-
-	const BitVector<3*N> binaryT(const BitVector<2*N> &x, const BitVector<2*N> &y) const{
-		const BitVector<4*N> & concatXY = BitVector<4*N>::template vCat<2*N, 2*N>(x, y);
-		return _gb2(_gb1(concatXY));
-	}
 };
 
 #endif
