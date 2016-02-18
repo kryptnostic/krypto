4a163e5bda6839df802946ce638262ca0f143f79
Hristo Oskov <hristo@kryptnostic.com>
2016-02-18 15:41:17 -0800

    renaming to krypto.js

--- krypto-lib/src/main/cpp/buildKryptoJS.sh
+++ krypto-lib/src/main/cpp/buildKryptoJS.sh
@@ -1,8 +1,8 @@
 # usage: buildKryptnosticEngineJS.sh
 FLAGS_PRE="-o"
 FLAGS_POST="--bind -std=c++11 -O3 --memory-init-file 0 -s TOTAL_STACK=10000000 -s DEMANGLE_SUPPORT=1 -ftemplate-depth=8192"
-EMSCRIPTEN="$EMSCRIPTEN_HOME/emscripten/tag-1.34.11/em++"
+EMSCRIPTEN="$EMSCRIPTEN_HOME/emscripten/1.35.0/em++"
 CPP_FILE="client/KryptnosticClientGetter.cpp"
-JS_FILE="$KRYPTO_JS_HOME/KryptnosticClient.js"
+JS_FILE="$KRYPTO_JS_HOME/krypto.js"
 CMD="$EMSCRIPTEN $FLAGS_PRE $JS_FILE $FLAGS_POST $CPP_FILE"
 eval $CMD
